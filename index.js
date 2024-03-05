166d81f3c35ac8c9db92318ff4b284b234a79920
Sir1ys <sashakoroviy@gmail.com>
2024-03-05 11:56:00 +0000

    test for failing the game is written, functionality is added

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-function startTheGame(userNum, randomNum) {
-  console.log(userNum, randomNum);
+function startTheGame(userNum, randomNum, attemptCount) {
   if (userNum === randomNum) return "Congratulations! Your number is correct!";
+
+  if (attemptCount > 3) return "Sorry, you lost the game!";
+  attemptCount += 1;
+
+  return startTheGame(userNum, randomNum, attemptCount);
 }
 
 function guessTheNumber(userNum) {
+  let attemptCount = 0;
   const randomNumber = Math.floor(Math.random() * 4);
 
-  return startTheGame(userNum, randomNumber);
+  return startTheGame(userNum, randomNumber, attemptCount);
 }
 
 module.exports = guessTheNumber;
--- index.test.js
+++ index.test.js
@@ -4,4 +4,8 @@ describe("guessTheNumber", () => {
   test("return the correct message when the user guess the number", () => {
     expect(guessTheNumber(3)).toBe("Congratulations! Your number is correct!");
   });
+
+  test("return the correct message when the user lost the game", () => {
+    expect(guessTheNumber(3)).toBe("Sorry, you lost the game!");
+  });
 });
